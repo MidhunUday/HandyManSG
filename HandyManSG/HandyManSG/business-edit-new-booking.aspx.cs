f1509be7fd8b3df37ded22dabb00f23949914ab3
KohChingEe <110922902+KohChingEe@users.noreply.github.com>
2022-08-11 00:23:44 +0800

    Add business-edit-new-booking html code

---
+++ HandyManSG/HandyManSG/business-edit-new-booking.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HandyManSG
+{
+    public partial class business_edit_new_booking : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
