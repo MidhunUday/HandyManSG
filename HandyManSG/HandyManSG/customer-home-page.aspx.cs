a5846e5775eeac30b4cf32e1fe4da3c36b9a958e
MidhunUday <midhun.udayakumar@gmail.com>
2022-08-11 06:43:17 +0800

    added in the backend code for customer home page

--- HandyManSG/HandyManSG/customer-home-page.aspx.cs
+++ HandyManSG/HandyManSG/customer-home-page.aspx.cs
@@ -12,6 +12,86 @@ namespace HandyManSG
         protected void Page_Load(object sender, EventArgs e)
         {
 
+
+            if (!IsPostBack)
+            {
+
+                if (Session["userID"] != null)
+                {
+                    Session["selectedServiceCategory"] = "Cleaning";
+                }
+                else
+                {
+                    Response.Redirect("UserLogin.aspx");
+                }
+
+
+            }
+
+            HttpContext.Current.Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            HttpContext.Current.Response.Cache.SetNoServerCaching();
+            HttpContext.Current.Response.Cache.SetNoStore();
+
         }
+
+
+        protected void lnkPlumbing_Click(object sender, EventArgs e)
+        {
+
+            LinkButton lnkbtn = sender as LinkButton;
+
+            string selectedServiceCategory = lnkbtn.CommandArgument.ToString();
+
+            Session["selectedServiceCategory"] = selectedServiceCategory;
+
+            Response.Redirect("customer-view-services.aspx");
+
+        }
+
+
+        protected void lnkCleaning_Click(object sender, EventArgs e)
+        {
+
+            LinkButton lnkbtn = sender as LinkButton;
+
+            string selectedServiceCategory = lnkbtn.CommandArgument.ToString();
+
+            Session["selectedServiceCategory"] = selectedServiceCategory;
+
+            Response.Redirect("customer-view-services.aspx");
+
+        }
+
+
+        protected void lnkElectrical_Click(object sender, EventArgs e)
+        {
+
+            LinkButton lnkbtn = sender as LinkButton;
+
+            string selectedServiceCategory = lnkbtn.CommandArgument.ToString();
+
+            Session["selectedServiceCategory"] = selectedServiceCategory;
+
+            Response.Redirect("customer-view-services.aspx");
+
+        }
+
+
+        protected void lnkAircon_Click(object sender, EventArgs e)
+        {
+
+            LinkButton lnkbtn = sender as LinkButton;
+
+            string selectedServiceCategory = lnkbtn.CommandArgument.ToString();
+
+            Session["selectedServiceCategory"] = selectedServiceCategory;
+
+            Response.Redirect("customer-view-services.aspx");
+
+        }
+
+
+
+
     }
 }
